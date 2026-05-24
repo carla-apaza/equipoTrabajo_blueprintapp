d7c85e8281f4af7ae1c694cf912eff54124c3c63
carla-apaza <carlaapazavilla@gmail.com>
2026-05-24 17:19:37 -0400

    first commit

---
+++ blueprintapp/miembros/routes.py
@@ -0,0 +1,58 @@
+# Librerias a usar en el modulo
+from flask import request, render_template, redirect, url_for, Blueprint
+
+# Referencia a la base de datos
+from blueprintapp.app import db
+
+# Modelos con los que interactura el modulo
+from blueprintapp.miembros.models import Miembro
+
+bp_miembro = Blueprint("bp_miembro", __name__, template_folder="templates")
+
+
+@bp_miembro.route("/")
+def index():
+    miembros = Miembro.query.all()
+    return render_template("miembro/index.html", miembros=miembros)
+
+
+@bp_miembro.route("/create", methods=["GET", "POST"])
+def create():
+    if request.method == "GET":
+        return render_template("miembro/create.html")
+    elif request.method == "POST":
+        nombre = request.form.get("nombre")
+        email = request.form.get("email")
+        # Crear un objeto miembro
+        miembro = Miembro(nombre=nombre, email=email)
+        # Insertar en la bd a traves del ORM
+        db.session.add(miembro)
+        db.session.commit()
+        # Redireccion al listado de miembros
+        return redirect(url_for("bp_miembro.index"))
+
+
+# --- OPERACIÓN EDITAR ---
+@bp_miembro.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    # Buscamos al miembro por su ID principal o devolvemos error 404 si no existe
+    miembro = Miembro.query.get_or_404(id)
+
+    if request.method == "GET":
+        return render_template("miembro/edit.html", miembro=miembro)
+
+    elif request.method == "POST":
+        miembro.nombre = request.form.get("nombre")
+        miembro.email = request.form.get("email")
+
+        db.session.commit()  # Guardamos los cambios efectuados sobre el objeto
+        return redirect(url_for("bp_miembro.index"))
+
+
+# --- OPERACIÓN ELIMINAR ---
+@bp_miembro.route("/delete/<int:id>", methods=["POST"])
+def delete(id):
+    miembro = Miembro.query.get_or_404(id)
+    db.session.delete(miembro)  # Eliminamos el registro
+    db.session.commit()
+    return redirect(url_for("bp_miembro.index"))
